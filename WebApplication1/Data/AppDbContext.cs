a4f6c5267cff36ad40907567c2a9ba0ac173ac95
d0ndar <popov.m14a@gmail.com>
2025-06-09 00:02:55 +0500

    added IKM

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration _Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(_Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Organizers> Organizers { get; set; }
+        public DbSet<LocationEvent> LocationEvents { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Events> Events { get; set; }
+        
+    }
+}
